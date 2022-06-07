48d98fd481bd8a4453b4337d977c0769a2fa6a25
Jedeye <jody.septiawan5@gmail.com>
2022-06-07 11:52:56 +0700

    update

--- src/components/list.js
+++ src/components/list.js
@@ -1,1 +1,17 @@
-// code Here
+function List(props) {
+    console.log(props)
+
+    const name = props.name
+    const color = props.color
+    const year = props.year
+
+    return (
+        <div>
+            <h1>{name}</h1>
+            <p>{color} - <b>{year}</b></p>
+            
+        </div>
+    )
+}
+
+export default List
--- src/props.js
+++ src/props.js
@@ -1,11 +1,37 @@
 import React from "react";
 
-//Import Components
+import List from "./components/list";
 
 function Props() {
+
+  const ar = [324,56,3213,658,2132]
+
   return (
-    // Code Inside div
-    <div></div>
+    <div>
+
+      <img 
+        src="https://media.suara.com/pictures/970x544/2022/05/30/52558-nissan-z-safety-car-antara-suaradotcom.jpg" 
+        alt="" 
+      />
+
+      <List 
+        name="Mobil A" 
+        color="yellow" 
+        year={2013}
+        array={ar}
+      />
+      <List 
+        name="Mobil B" 
+        color="red" 
+        year={2015}
+      />
+      <List 
+        name="Mobil C" 
+        color="blue" 
+        year={2007}
+      />
+
+    </div>
   );
 }
 
