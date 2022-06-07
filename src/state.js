aab4e5db5f364d024bfc34231ac43604162e2adf
Jedeye <jody.septiawan5@gmail.com>
2022-06-07 12:06:31 +0700

    update

--- src/state.js
+++ src/state.js
@@ -1,15 +1,26 @@
-import React from "react";
+import { useState } from "react";
 
 function State() {
   //init State
+  const [number, setNumber] = useState(35);
 
   //Create Function Add
+  const Add = () => {
+    setNumber(number + 1)
+  }
 
   //Create Function Less
+  const Less = () => {
+    setNumber(number - 1)
+  }
 
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <h1>{number}</h1>
+      <button onClick={Add}>Add</button>
+      <button onClick={Less} >Less</button>
+    </div>
   );
 }
 
