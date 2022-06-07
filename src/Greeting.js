92927bd0dc95938bf7c40a9d8707f8e0588af809
Jedeye <jody.septiawan5@gmail.com>
2022-06-07 11:33:12 +0700

    update

---
+++ src/Greeting.js
@@ -0,0 +1,5 @@
+function Greeting() {
+    alert('Welcome all file: Greeting.js')
+}
+
+export default Greeting
--- src/event.js
+++ src/event.js
@@ -1,11 +1,19 @@
 import React from "react";
+// import Greeting from "./Greeting";
 
 function Event() {
   //Create function here
+  function Greeting() {
+      alert('Welcome all file: Greeting.js')
+  }
 
   return (
-    // Code Inside div
-    <div></div>
+    <div>
+      <p>If you press "Click Me" then an alert will appear</p>
+      <button onClick={() => alert('Hello Full-Stack')}>Click Me</button>
+      
+      <button onClick={Greeting}>Greeting</button>
+    </div>
   );
 }
 
