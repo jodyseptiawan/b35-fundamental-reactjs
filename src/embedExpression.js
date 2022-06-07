abb1715272c0515bd22a21a8529d63db83c641a0
Jedeye <jody.septiawan5@gmail.com>
2022-06-07 11:20:32 +0700

    update

--- src/embedExpression.js
+++ src/embedExpression.js
@@ -3,11 +3,21 @@ import React from "react";
 function EmbedExpression() {
   //Create function here
 
+  function getMajor() {
+    return " Full-Stack"
+  }
+
   //Create a variable here
+  const companyName = "Dumbways Indonesia"
+
+  const batch = 'Batch 35'
 
   return (
-    // Code Inside div
-    <div></div>
+    <div>
+      <p>
+        Welcome to {companyName} Class {getMajor()} {batch}
+      </p>
+    </div>
   );
 }
 
